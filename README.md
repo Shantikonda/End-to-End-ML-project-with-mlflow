a8c77816c9cc79b1a7c244fa9cd109ff8f734c25
shanti <skgeo12@gmail.com>
2024-05-26 15:49:11 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # End-to-End-ML-project-with-mlflow
+
+# Git commands
+# git commit -m "<text>"
+# git push origin main #pushes the repo to GitHub
--- template.py
+++ template.py
@@ -13,14 +13,14 @@ list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/utils/common.py", # for all utilities
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
-    "config/config.yaml",
+    "config/config.yaml", # for all configurations of project
     "params.yaml",
     "schema.yaml",
     "main.py",
